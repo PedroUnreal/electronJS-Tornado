623de6440611766c8f01b7b86d1e13a7b26270da
PedroUnreal <pedrounreal@gmail.com>
2021-12-04 18:21:09 +0300

    small fix

--- renderer.js
+++ renderer.js
@@ -1,14 +1,18 @@
-
-function renderJson(json){
-const h1 = document.querySelector("h1");
-const div = document.createElement("div");
-h1.appendChild(div).innerText = json;
+function renderJson(json) {
+  const h1 = document.querySelector("h1");
+  const div = document.createElement("div");
+  if (h1) {
+    h1.appendChild(div).innerText = json;
+  }
 }
 
 async function getJson() {
-  let json = await fetch("http://localhost:8080/api");
-  json = await json.json();
-  renderJson(json.test)  
+  try {
+    let json = await fetch("http://localhost:8080/api");
+    json = await json.json();
+    renderJson(json.test);
+  } catch (e) {
+    console.log(e);
+  }
 }
 getJson();
-
